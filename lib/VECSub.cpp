b09b4cab205789535a39d39c6eaa56571ebbd853
Hamidreza Sanaee <shrsanaee@gmail.com>
2021-09-24 12:01:47 +0330

    Fix sum temp and add carry_temp instruction

--- lib/VECSub.cpp
+++ lib/VECSub.cpp
@@ -44,6 +44,8 @@ bool VECSub::runOnBasicBlock(BasicBlock &BB) {
 
   // Loop over all instructions in the block. Replacing instructions requires
   // iterators, hence a for-range loop wouldn't be suitable.
+  auto Val_Carry_temp = ConstantInt::get(BB.getContext(), llvm::APInt(64,
+                                              0xffffffffffffffff, false));
   for (auto Inst = BB.begin(), IE = BB.end(); Inst != IE; ++Inst) {
 
     // Skip non-binary (e.g. unary or compare) instruction.
@@ -60,26 +62,29 @@ bool VECSub::runOnBasicBlock(BasicBlock &BB) {
     // them into basic blocks.
     IRBuilder<> Builder(BinOp);
     // Constants used in building the instruction for substitution
-    auto Val_Carry_temp = ConstantInt::get(BinOp->getContext(), llvm::APInt(64,  
-                                              0xffffffffffffffff, false));
+   // auto Val_Carry_temp = ConstantInt::get(BinOp->getContext(), llvm::APInt(64,  
+     //                                         0xffffffffffffffff, false));
     // auto Oprand_One_64Cast= ConstantInt::get(BinOp->getContext(), llvm::APInt(64,  
     //                                           BinOp->getOperand(1)->getvalue, true));
     // Create an instruction representing t0 = ~vin1[0]
-    Instruction *PartOne = BinaryOperator::CreateXor(BinOp->getOperand(1),
-                                             ConstantInt::get(BinOp->getContext(),llvm::APInt(64,-1, false)));
+    Instruction *PartOne = BinaryOperator::CreateAnd(BinOp->getOperand(0),
+		    			Builder.CreateAnd(Val_Carry_temp,
+                                             Builder.CreateNot(BinOp->getOperand(1))));
+    //Val_Carry_temp = BinOp->getOperand(1) & Val_Carry_temp;
     // Create an instruction representing sum_temp = vin0[0]  ^ t0 ^ carry_temp
-    Instruction *PartTwo = BinaryOperator::CreateXor(BinOp->getOperand(0),
-                                              Builder.CreateXor(PartOne,Val_Carry_temp));
+    Instruction *PartTwo = BinaryOperator::CreateOr(Builder.CreateAnd(BinOp->getOperand(0),Builder.CreateNot(BinOp->getOperand(1))),
+                                              Builder.CreateOr(Builder.CreateAnd(BinOp->getOperand(0),Val_Carry_temp),Builder.CreateAnd(Builder.CreateNot(BinOp->getOperand(1)),Val_Carry_temp)));
+
     // Create an instruction representing carry_temp
     // Instruction *PartCarry = 
     // Instruction *NewValue = BinaryOperator::CreateXor(
     //     BinOp->getOperand(1),);
         
     // Create an instruction representing (a + ~b) + 1
-    Instruction *NewValue = BinaryOperator::CreateAdd(
-        Builder.CreateAdd(BinOp->getOperand(0),
-                          Builder.CreateNot(BinOp->getOperand(1))),
-        ConstantInt::get(BinOp->getType(), 1));
+   // Instruction *NewValue = BinaryOperator::CreateAdd(
+    //    Builder.CreateAdd(BinOp->getOperand(0),
+      //                    Builder.CreateNot(BinOp->getOperand(1))),
+       // ConstantInt::get(BinOp->getType(), 1));
 
     // The following is visible only if you pass -debug on the command line
     // *and* you have an assert build.
